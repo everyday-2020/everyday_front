af66852589e66a1b080b730f0f5d9506cf84c1ff
jooseung <jsedwardyou@gmail.com>
2020-05-15 02:56:28 +0900

    add prettier in setup.sh and in pre-commit

--- setup.sh
+++ setup.sh
@@ -8,3 +8,12 @@ ncu -u
 npm install
 
 yarn install
+
+# prettier missing issue after yarn install
+# https://github.com/rails/webpacker/issues/1212
+# install prettier manually
+
+npm install -g prettier
+
+# husky: runs prettier on pre-commit
+yarn add pretty-quick husky --dev
