d2da615615406b3911b898ee3776ed9d1b221f5f
Jang Whemoon <palindrom615@gmail.com>
2020-05-17 16:51:26 +0900

    Add room

--- src/containers/room.tsx
+++ src/containers/room.tsx
@@ -1,3 +1,18 @@
 import React from "react";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import AddBoxOutlinedIcon from '@material-ui/icons/AddBoxOutlined';
 
-export default () => <></>;
+export default () => {
+  return (
+    <>
+      <div className="content" style={{ height: "3000px" }}></div>
+      <BottomNavigation
+        style={{ position: "sticky", bottom: 0, backgroundColor: "#f0f0f0" }}
+        showLabels
+      >
+        <BottomNavigationAction label="Upload" icon={<AddBoxOutlinedIcon />} />
+      </BottomNavigation>
+    </>
+  );
+};
